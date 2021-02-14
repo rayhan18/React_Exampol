45473c10dbcfde08371b9174304bb4d06488ace6
rayhan18 <rabu4960@gmail.com>
2021-02-14 08:44:12 +0300

    functional component

--- my-app/src/Component/FunctionalPropse.js
+++ my-app/src/Component/FunctionalPropse.js
@@ -1,8 +1,29 @@
 import React from'react';
 
 function FunctionalPropse(props){
+    const persons=['hanif','korim','roim','jodo','modo']
+
+
     return(
+        <div>
         <h1>name:{props.name} age:{props.age}</h1>
+       <ul>
+           {
+                persons.map(person=> <li>{person}</li>) 
+           }
+           
+       </ul>
+       </div>
+    )
+}
+
+
+
+
+
+function person(params) {
+    return(
+       <h1>rayhan</h1>
     )
 }
 export default FunctionalPropse;
--- my-app/src/Component/State/Sestste.js
+++ my-app/src/Component/State/Sestste.js
@@ -1,28 +1,47 @@
 
 
 import React, {Component}from 'react';
-import {Container}from'react-bootstrap';
+import {Container,Button}from'react-bootstrap';
 
 
-class Sestste extends Component{
+class Sestste extends Component {
 
-    constructor(){
+    constructor() {
         super()
-        this.state={
-            massege:'WelCome use setstaate'
+        this.state = {
+            massege: 'WelCome use setstaate',
+            name: 'rayhan',
+            count:0
+
         }
     }
-    onchange(){
+
+    onchange() {
         this.setState({
-            massege:'thank you' //object change with function
+            massege: 'thank you' //object change with function
         })
     }
+
+    nameChange(a) {
+        this.setState({name:a})
+
+    }
+
     render(){
         return(
            <div >
+
                <Container>
                <h1> this is setstates {this.state.massege}</h1>
                <button onClick={()=>this.onchange()}> button</button>
+                   <h1> this name change{this.state.name}</h1>
+                   <Button onClick={this.nameChange.bind(this, "molla")}> button</Button>
+
+
+                   <div>
+                       <h2>{this.state.count}</h2>
+                       <Button onClick={ ()=> {this.setState({count: this.state.count+1})}}>buttoncoutn</Button>
+                   </div>
                </Container>
            
            </div>
